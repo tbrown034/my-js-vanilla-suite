daeb82d73a43ff03e942bef6459d5cb30a71d2c9
tbrown034 <tbrown034@gmail.com>
2023-11-16 20:39:03 -0500

    feat: added select your game with buttons, no functionality

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("hello");
+// Selectors
+
+const homePage = document.getElementById("hero-page");
+const themeButton = document.getElementById("theme-button");
+const learnMore = document.getElementById("learn-more");
+const RandomGame = document.getElementById("random-game");
+const ticSelect = document.getElementById("tic-select");
+const connectSelect = document.getElementById("connect-select");
+const rockSelect = document.getElementById("rock-select");
+const ticSection = document.getElementById("tic-section");
+const startButton = document.getElementsByClassName("start-button");
+const backButton = document.getElementsByClassName("back-button");
+const turnTracker = document.getElementsByClassName("turn-tracker");
